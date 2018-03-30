bb2b3780018e9d7f0789cf7cdcf641782086c28f
hadleigh-000 <hadleigh.nunes@students.olin.edu>
2018-03-30 14:08:10 -0400

    classes

--- HNdraft.py
+++ HNdraft.py
@@ -6,4 +6,29 @@ import time
 
 
 class Backdrop(object):
-    def __init__(self, )
+    def __init__(self, ):
+
+class Item(pygame.sprite.Sprite):
+    def __init__(self, loc):
+        pygame.sprite.Sprite.__init__(self)
+
+class Character(pygame.sprite.Sprite):
+    def __init__(self, loc):
+        pygame.sprite.Sprite.__init__(self)
+
+class Message(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+class Scene(object):
+    def __init__(self):
+
+
+class GameModel(object):
+    def __init__(self):
+
+
+class PygameWindowView(object):
+    def __init__(self):
+
+        
