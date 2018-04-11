4e34c620db1116a9fb80c228a0a830e7298d8640
hadleigh-000 <hadleigh.nunes@students.olin.edu>
2018-04-11 19:47:24 -0400

    adding a bit of structure, and doing items

--- HNdraft.py
+++ HNdraft.py
@@ -11,8 +11,8 @@ class Backdrop(object):
         self.image = pygame.image.load(image_name)
         self.rect = self.image.get_rect()
 
-    def draw(self, scrn, loc):
-        scrn.blit(self.image, loc = (0,0))
+    def draw(self, scrn, loc = (0,0)):
+        scrn.blit(self.image, loc)
         #pygame.display.update()
 
 # class Background(pygame.Surface):
@@ -24,32 +24,44 @@ class Backdrop(object):
 #      def draw(self):
 #         pygame.display.update()
 
-class Textbox(pygame.sprite.Sprite):
+class Textbox(pygame.Surface):
 
-    def __init__(self, color, width, height):
-       # Call the parent class (Sprite) constructor
-       pygame.sprite.Sprite.__init__(self)
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+     def __init__(self,width,height):
+        pygame.Surface.__init__(self,(width,height))
+        pygame.sprite.Sprite.__init__(self)
 
-       # Create an image of the block, and fill it with a color.
-       # This could also be an image loaded from the disk.
-       self.image = pygame.Surface([width, height])
-       self.image.fill(color)
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.imageout = pygame.Surface((width,height))
+        self.imageout.fill((255,0,0))
 
-       # Fetch the rectangle object that has the dimensions of the image
-       # Update the position of this object by setting the values of rect.x and rect.y
-       self.rect = self.image.get_rect
+        # Fetch the rectangle object that has the dimensions of the image
+        # Update the position of this object by setting the values of rect.x and rect.y
+        self.rectout = self.imageout.get_rect()
+        self.rectout.x=0
+        self.rectout.y=1000
 
-    def zewords(self,text):
-        pygame.font.init()
-        myfont = pygame.font.SysFont('Comic Sans MS', 28)
-        textsurface = myfont.render(text)
-        self.image.blit(textsurface)
+        pygame.sprite.Sprite.__init__(self)
+
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.imagein = pygame.Surface((width*0.9,height*0.75))
+        self.imagein.fill((0,255,0))
+
+        # Fetch the rectangle object that has the dimensions of the image
+        # Update the position of this object by setting the values of rect.x and rect.y
+        self.rectin = self.imagein.get_rect()
+        self.rectin.x=0
+        self.rectin.y=1000
 
 
 class Item(pygame.sprite.Sprite):
     def __init__(self, img, loc):
         pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image(img,-1) #see if needs fixing
+        self.image= pygame.image.load(img) #see if needs fixing
+        self.rect = self.image.get_rect()
         self.loc = loc
         self.clicked = False
 
@@ -68,19 +80,23 @@ class Character(pygame.sprite.Sprite):
 class Inventory(pygame.sprite.Group):
     def __init__(self):
         pygame.sprite.Group.__init__(self)
-
+        self.items = []
+    def add_item(self, item):
+        self.items.append(item)
 
 class Room(pygame.sprite.LayeredUpdates):
-    def __init__(self, msgs, items, scrn):
+    def __init__(self, msgs, items, backdrops):
         pygame.sprite.LayeredUpdates.__init__(self)
         self.messages = msgs
         self.items = items
+        self.backdrop = backdrops
         self.items_vis = []
         for i in range(len(items)):
-            if items[i].beenclicked == False:
+            if items[i].clicked == False:
                 self.items_vis.append(items[i])
 
     def draw(self, scrn):
+        self.backdrop.draw(scrn)
         for item in self.items_vis:
             item.draw(scrn)
 
@@ -89,25 +105,26 @@ class Room(pygame.sprite.LayeredUpdates):
             item.update(click_pos)
 
 
-class GameModel(object):
-    def __init__(self):
-
-        self.scenes = []
+class SpaceGameModel(object):
+    def __init__(self, rooms):
+        self.allrooms = rooms
+        self.room = self.allrooms["bridge"]
         self.inventory = Inventory()
 
+    def draw(self, scrn):
+        self.room.draw(scrn)
 
 class PygameWindowView(object):
-    def __init__(self, width = 640,height = 480):
+    def __init__(self, model, width = 640,height = 480):
+        pygame.init()
         size = (width,height)
         pygame.display.init()
         self.screen = pygame.display.set_mode(size)
-        self.backdrop = backdrp
+        self.model = model
 
-    def draw(self, model):
-
+    def draw(self):
+        self.model.draw(self.screen)
         pygame.display.update()
-
-
 
 class MouseController(object):
     def __init__(self, model):
@@ -115,5 +132,17 @@ class MouseController(object):
 
 if __name__ == '__main__':
     pygame.init()
-    stock = Backdrop("StockPhoto1.jpg")
-    SCRNtemp = PygameWindowView(stock)
+    running = True
+    while running:
+        kiwi = Item("Wrench.jpg", (0,0))
+        stock = Backdrop("StockPhoto1.jpg")
+        room =  Room({}, [kiwi], stock)
+        rooms = {"bridge":room}
+        Modl = SpaceGameModel(rooms)
+
+        SCRNtemp = PygameWindowView(Modl)
+        SCRNtemp.draw()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running=False
