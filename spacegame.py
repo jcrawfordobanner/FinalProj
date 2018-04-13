a8ada0c0bd2d0d9f8db3978375ade91be536933f
cusaitech <cusaitech@gmail.com>
2018-04-13 14:47:11 -0400

    Update master file

--- spacegame.py
+++ spacegame.py
@@ -9,18 +9,24 @@ from classes import *
 
 if __name__ == '__main__':
     pygame.init()
+
+    wrench = Item("wrench.png", (200,200))
+    wrench.image = pygame.transform.scale(wrench.image, (50, 60))
+    stock = Backdrop("StockPhoto1.jpg", (640, 480))
+    room =  Room({}, [wrench], stock)
+    rooms = {"bridge":room}
+
+    Modl = SpaceGameModel(rooms)
+    SCRNtemp = PygameWindowView(Modl)
+    Contrl = MouseController(Modl)
+
     running = True
     while running:
-        wrench = Item("wrench.png", (200,200))
-        wrench.image = pygame.transform.scale(wrench.image, (50, 60))
-        stock = Backdrop("StockPhoto1.jpg", (640, 480))
-        room =  Room({}, [wrench], stock)
-        rooms = {"bridge":room}
-        Modl = SpaceGameModel(rooms)
-
-        SCRNtemp = PygameWindowView(Modl)
-        SCRNtemp.draw()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running=False
+        Modl.update()
+        SCRNtemp.draw()
+        time.sleep(.001)
+
+    pygame.quit()
