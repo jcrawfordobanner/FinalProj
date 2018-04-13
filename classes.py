570808485859ab9d4e3363e467aa8392f7468455
hadleigh-000 <hadleigh.nunes@students.olin.edu>
2018-04-13 14:22:17 -0400

    adding the classes from HN doc

---
+++ classes.py
@@ -0,0 +1,95 @@
+import pygame
+from pygame.locals import*
+import time
+
+
+class Backdrop(object):
+    def __init__(self, image_name, size):
+
+        im = pygame.image.load(image_name)
+        self.image = pygame.transform.scale(im, size)
+        self.rect = self.image.get_rect()
+
+    def draw(self, scrn, loc = (0,0)):
+        scrn.blit(self.image, loc)
+
+
+class Textbox(pygame.Surface):
+
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+     def __init__(self,width,height):
+        pygame.Surface.__init__(self,(width,height))
+        pygame.sprite.Sprite.__init__(self)
+
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.imageout = pygame.Surface((width,height))
+        self.imageout.fill((255,0,0))
+
+        # Fetch the rectangle object that has the dimensions of the image
+        # Update the position of this object by setting the values of rect.x and rect.y
+        self.rectout = self.imageout.get_rect()
+        self.rectout.x=0
+        self.rectout.y=1000
+
+        pygame.sprite.Sprite.__init__(self)
+
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.imagein = pygame.Surface((width*0.9,height*0.75))
+        self.imagein.fill((0,255,0))
+
+        # Fetch the rectangle object that has the dimensions of the image
+        # Update the position of this object by setting the values of rect.x and rect.y
+        self.rectin = self.imagein.get_rect()
+        self.rectin.x=0
+        self.rectin.y=1000
+
+
+class Item(pygame.sprite.Sprite):
+    def __init__(self, img, loc):
+        pygame.sprite.Sprite.__init__(self)
+        self.image= pygame.image.load(img) #see if needs fixing
+        self.rect = self.image.get_rect()
+        self.loc = loc
+        self.clicked = False
+
+    def draw(self, scrn):
+        scrn.blit(self.image, self.loc)
+
+
+    def update(self, click_pos):
+        if self.loc == click_pos:
+            self.clicked = True
+
+class Character(pygame.sprite.Sprite):
+    def __init__(self, loc):
+        pygame.sprite.Sprite.__init__(self)
+
+class Inventory(pygame.sprite.Group):
+    def __init__(self):
+        pygame.sprite.Group.__init__(self)
+        self.items = []
+    def add_item(self, item):
+        self.items.append(item)
+
+class Room(pygame.sprite.LayeredUpdates):
+    def __init__(self, msgs, items, backdrops):
+        pygame.sprite.LayeredUpdates.__init__(self)
+        self.messages = msgs
+        self.items = items
+        self.backdrop = backdrops
+        self.items_vis = []
+        for i in range(len(items)):
+            if items[i].clicked == False:
+                self.items_vis.append(items[i])
+
+    def draw(self, scrn):
+        self.backdrop.draw(scrn)
+        for item in self.items_vis:
+            item.draw(scrn)
+
+    def update(self, click_pos):
+        for item in self.items.sprites():
+            item.update(click_pos)
