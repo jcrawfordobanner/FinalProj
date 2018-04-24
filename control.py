675965e07316d5384f727ef3ed42208354929276
jcrawfordobanner <jordan.crawford-obanner@students.olin.edu>
2018-04-24 18:38:08 -0400

    website

--- control.py
+++ control.py
@@ -1,11 +1,12 @@
 #Dictates how the player can interact with the model
 from model import *
+import pygame
 class MouseController(object):
     """ Handles input for space game """
     def __init__(self, model):
         self.model = model
 
-    def handle_event(self, event):
+    def handle_event(self, event,scren):
         """ Event handler"""
         if event.type == MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
@@ -15,28 +16,60 @@ class MouseController(object):
             return
         if self.model.eventflags.get('1')==True:
             self.model.textbox.update(self.model.messages['game_intro'])
+            scren.draw()
             if event.key == pygame.K_j:
-                self.model.textbox.update(self.model.messages['scene1'])
                 self.model.eventflags['1']=False
+                self.model.textbox.update(self.model.messages['scene1'])
+                scren.draw()
                 self.model.eventflags['2']=True
+                pygame.time.wait(2005)
+                self.model.textbox.update(self.model.messages['bridge_intro'])
+                scren.draw()
             if event.key == pygame.K_k:
                 self.model.eventflags['1']=False
                 self.model.textbox.update(self.model.messages['scene2'])
+                scren.draw()
             if event.key == pygame.K_l:
                 self.model.textbox.update(self.model.messages['scene3'])
+                scren.draw()
                 self.model.eventflags['1']=False
-            return
 
-        if self.model.eventflags.get('2')==True:
-            self.model.textbox.update(self.model.messages['game_intro'])
+        elif self.model.eventflags.get('2')==True:
+            pygame.time.wait(2005)
+            self.model.textbox.update(self.model.messages['bridge_intro'])
+            scren.draw()
             if event.key == pygame.K_j:
-                self.model.textbox.update(self.model.messages['scene1'])
-                self.model.eventflags['1']=False
-                self.model.eventflags['2']=True
+                pygame.time.wait(2005)
+                self.model.textbox.update(self.model.messages['scene4'])
+                scren.draw()
+                self.model.eventflags['2']=False
             if event.key == pygame.K_k:
-                self.model.eventflags['1']=False
-                self.model.textbox.update(self.model.messages['scene2'])
+                pygame.time.wait(2005)
+                self.model.eventflags['2']=False
+                self.model.textbox.update(self.model.messages['scene5'])
+                scren.draw()
             if event.key == pygame.K_l:
-                self.model.textbox.update(self.model.messages['scene3'])
-                self.model.eventflags['1']=False
-            return
+                pygame.time.wait(2005)
+                self.model.textbox.update(self.model.messages['scene6'])
+                scren.draw()
+                self.model.eventflags['2']=False
+
+        elif self.model.eventflags.get('2')==True:
+            pygame.time.wait(2005)
+            self.model.textbox.update(self.model.messages['bridge_intro'])
+            scren.draw()
+            if event.key == pygame.K_j:
+                pygame.time.wait(2005)
+                self.model.textbox.update(self.model.messages['scene4'])
+                scren.draw()
+                self.model.eventflags['2']=False
+            if event.key == pygame.K_k:
+                pygame.time.wait(2005)
+                self.model.eventflags['2']=False
+                self.model.textbox.update(self.model.messages['scene5'])
+                scren.draw()
+            if event.key == pygame.K_l:
+                pygame.time.wait(2005)
+                self.model.textbox.update(self.model.messages['scene6'])
+                scren.draw()
+                self.model.eventflags['2']=False
--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 #Keeps track of all of the variables being changed in each scene
 from classes import *
-import narrative.py
+import narrative
 class SpaceGameModel(object):
     """Model of the game"""
     def __init__(self, size, rooms):
@@ -8,12 +8,7 @@ class SpaceGameModel(object):
         self.room = self.allrooms["bridge"]
         self.inventory = Inventory()
         self.eventflags = {'1':True,'2':False,'3':False,'4':False,'5':False}
-        self.messages = {"bridge_intro":('You are now in the bridge', 'There is a paper clip and toothbrush and stapler',
-                'j:paper clip k:toothbrush l:stapler'),
-                "scene1":('You pressed the red button', 'A door opens to your right'),
-                'scene2':('You pressed the green button', 'Unfortunately nothing happened. You stay until you die of thirst', 'Game Over'),
-                "scene3":('You pressed the blue button', 'Congratulations...you suck', 'Game Over'),
-                'game_intro': ('You have awoke inside of a room.', 'In it you see three buttons, one red, one blue and one green.', 'What do you do?', 'j: red k: blue l: green')}
+        self.messages= narrative.messages
  # dictionary of all possible messages to be displayed on the textbox
         self.textbox= Textbox(size, self.messages['game_intro'])
 
